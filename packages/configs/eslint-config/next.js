10897c196cef473920d9157f1ab5755d2c34c538
Hashversion <110537156+Hashversion@users.noreply.github.com>
2025-10-21 22:55:36 +0530

    feat: init apps/web with create-next-app

--- packages/configs/eslint-config/next.js
+++ packages/configs/eslint-config/next.js
@@ -3,9 +3,9 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginNext from "eslint-plugin-next";
+import pluginNext from "@next/eslint-plugin-next";
 import { defineConfig } from "eslint/config";
-import baseConfig from "./base";
+import baseConfig from "./base.js";
 
 export default defineConfig([
   ...baseConfig,
