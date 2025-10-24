4d817808ccfc87955fde6c8214dd8ca5c9287724
Hashversion <110537156+Hashversion@users.noreply.github.com>
2025-10-24 23:43:24 +0530

    feat: setup client-side posthog web analytics

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,21 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // posthog proxy
+      {
+        source: "/_proxy/posthog/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/_proxy/posthog/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 initOpenNextCloudflareForDev();
