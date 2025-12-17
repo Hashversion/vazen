23c828a5c64fcc229d4512c9f516332a64ec1162
Hashversion <110537156+Hashversion@users.noreply.github.com>
2025-12-17 17:33:46 +0530

    chore: remove db generated migration files from repo

--- packages/db/drizzle/0000_quick_cargill.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "waitlist" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"joined_at" timestamp with time zone DEFAULT now() NOT NULL,
-	CONSTRAINT "waitlist_email_unique" UNIQUE("email")
-);
