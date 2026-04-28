b8d8e0efc9256cd52f89bd00e6d0079518fda57d
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-04-28 17:44:04 +0530

    feat: add NotFound component for 404 error page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@repo/ui/";
+
+export default function NotFound() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">404</h1>
+      <p className="mt-2 text-2xl">Page not found</p>
+
+      <Button className="mt-4" nativeButton={false} render={<Link href="/" />}>
+        Go to Home
+      </Button>
+    </main>
+  );
+}
